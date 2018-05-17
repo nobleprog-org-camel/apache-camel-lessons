fcd13fd00f0df3f42df6ac8f653f1dd0992777b8
sahdev <sahdevghorela37@gmail.com>
2018-05-17 13:54:51 +0100

    test

--- camel-components/camel-jdbc/src/main/java/org/nobleprog/camel/component/jdbc/route/RestRoute.java
+++ camel-components/camel-jdbc/src/main/java/org/nobleprog/camel/component/jdbc/route/RestRoute.java
@@ -1,14 +1,21 @@
 package org.nobleprog.camel.component.jdbc.route;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestRoute extends RouteBuilder {
+    @Autowired
+    private CamelContext camelContext;
 
     public void configure() throws Exception {
+        //TODO: Uncomment below to see the tracing of our route
+        //camelContext.setTracing(true);
+
         from("direct:start")
                 .bean(OrderToSqlBean.class, "createOrderSql")
                 .to("jdbc:dataSource")
